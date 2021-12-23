c0bdbea231723c87bb883e36f35a95b6d62d18c6
Manezki <manezki@gmail.com>
2021-12-23 21:16:14 +0100

    Feat: Day 16 puzzle 2 completed

--- src/day_16.rs
+++ src/day_16.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::{fs};
 
 #[derive(Debug)]
@@ -6,11 +7,48 @@ enum Packet {
     Literal(Literal)
 }
 
+impl Value for Packet {
+    fn value(&self) -> u128 {
+        let ret;
+        match self {
+            Packet::Operator(op) => ret = op.value(),
+            Packet::Literal(lit) => ret= lit.value(),
+        }
+        return ret;
+    }
+}
+
 #[derive(Debug)]
 struct Operator {
     version: u8,
     type_id: u8,
     sub_packets: Vec<Packet>
+}
+
+trait Value {
+    fn value(&self) -> u128;
+}
+
+impl Value for Operator {
+    fn value(&self) -> u128 {
+        match self.type_id {
+            0 => return self.sub_packets.iter().fold(0, |acc, e| acc + e.value()),
+            1 => return self.sub_packets.iter().fold(1, |acc, e| acc * e.value()),
+            2 => return self.sub_packets.iter().fold(u128::MAX, |acc, e| acc.min(e.value())),
+            3 => return self.sub_packets.iter().fold(0, |acc, e| acc.max(e.value())),
+            5 => return if self.sub_packets[0].value() > self.sub_packets[1].value() { 1 } else { 0 },
+            6 => return if self.sub_packets[0].value() < self.sub_packets[1].value() { 1 } else { 0 },
+            7 => return if self.sub_packets[0].value() == self.sub_packets[1].value() { 1 } else { 0 },
+            _ => panic!("Unrecognized packet type {:?}", self.type_id)
+        }
+    }
+    
+}
+
+impl Value for Literal {
+    fn value(&self) -> u128 {
+        return self.value;
+    }
 }
 
 #[derive(Debug)]
@@ -130,7 +168,6 @@ fn version_count (packets: Vec<Packet>) -> u32 {
         match packet {
             Packet::Operator(op) => version_total += op.version as u32 + version_count(op.sub_packets),
             Packet::Literal(lit) => version_total += lit.version as u32,
-            _ => panic!("Unrecognized package {:?}", packet)
         }
     }
 
@@ -138,15 +175,42 @@ fn version_count (packets: Vec<Packet>) -> u32 {
 }
 
 #[test]
-fn puzzle_1_confers_to_example() {
+fn puzzle_1_confers_to_examples() {
     assert_eq!(version_count(vec![parse_next_packet(to_binary("8A004A801A8002F478".to_string())).0]), 16);
     assert_eq!(version_count(vec![parse_next_packet(to_binary("620080001611562C8802118E34".to_string())).0]), 12);
     assert_eq!(version_count(vec![parse_next_packet(to_binary("C0015000016115A2E0802F182340".to_string())).0]), 23);
     assert_eq!(version_count(vec![parse_next_packet(to_binary("A0016C880162017C3686B18A3D4780".to_string())).0]), 31);
 }
 
+#[test]
+fn puzzle_2_confers_to_examples() {
+
+    let examples = [
+        ("C200B40A82".to_string(), 3),
+        ("04005AC33890".to_string(), 54),
+        ("880086C3E88112".to_string(), 7),
+        ("CE00C43D881120".to_string(), 9),
+        ("D8005AC2A8F0".to_string(), 1),
+        ("F600BC2D8F".to_string(), 0),
+        ("9C005AC2F8F0".to_string(), 0),
+        ("9C0141080250320F1802104A08".to_string(), 1),
+        ];
+
+    for (transmission, value) in examples {
+        match parse_next_packet(to_binary(transmission)).0 {
+            Packet::Operator(op) => assert_eq!(op.value(), value),
+            _ => panic!("Initial packet was NOT Operator")
+        }
+    }
+}
+
 fn main () {
     let file_content = fs::read_to_string("src\\day_16_input.txt").expect("Something went wrong reading the file");
     let binary_transmission = to_binary(file_content);
-    println!("{:?}", version_count(vec![parse_next_packet(binary_transmission).0]));
+
+    let (package, _) = parse_next_packet(binary_transmission.clone());
+    println!("{:?}", version_count(vec![package]));
+
+    let (package, _) = parse_next_packet(binary_transmission);
+    println!("{:?}", package.value());
 }
